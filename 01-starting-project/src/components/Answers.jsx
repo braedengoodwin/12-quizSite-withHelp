0bf874b32d5fac6a26f8969bd209b3be9a14d436
braedengoodwin <braedenagoodwin@hotmail.com>
2024-04-05 17:21:20 -0500

    finished quiz project

---
+++ 01-starting-project/src/components/Answers.jsx
@@ -0,0 +1,50 @@
+import { useRef } from "react";
+import React from "react";
+
+export default function Answers({
+  answers,
+  selectedAnswer,
+  answerState,
+  onSelect,
+}) {
+  const shuffledAnswers = useRef();
+
+  if (!shuffledAnswers.current) {
+    shuffledAnswers.current = [...answers];
+    // this is swappping the elements if you return a neg number
+    // if you return a positive number they will stay in the order they are
+    shuffledAnswers.current.sort(() => Math.random() - 0.5);
+  }
+
+  return (
+    <ul id="answers">
+      {shuffledAnswers.current.map((answer) => {
+        let cssClass = "";
+        const isSelected = selectedAnswer === answer;
+
+        if (answerState === "answered" && isSelected) {
+          cssClass = "selected";
+        }
+
+        if (
+          (answerState === "correct" || answerState === "wrong") &&
+          isSelected
+        ) {
+          cssClass = answerState;
+        }
+
+        return (
+          <li key={answer} className="answer">
+            <button
+              onClick={() => onSelect(answer)}
+              className={cssClass}
+              disabled={answerState !== ''}
+            >
+              {answer}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
---
+++ 01-starting-project/src/components/QuestionTimer.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import React from "react";
+
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onTimeout();
+    }, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  // eslint-disable-next-line react/react-in-jsx-scope
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
+}
